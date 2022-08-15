9b9f4a115294b11d212fe7b391dcad5d3abbc578
theemadnes <alex.mattson@gmail.com>
2022-08-14 22:52:57 -0500

    more cleanup

--- main.go
+++ main.go
@@ -41,35 +41,11 @@ func generateDeploymentSpec(stream []uint8) string {
 	// copy podspec from YAML import to new pod
 	pod_1.Spec = rev.DeepCopy().Spec.PodSpec
 
-	//fmt.Printf("%+v", *rev)
-	//fmt.Println(reflect.TypeOf(*rev))
-	//fmt.Println(rev.TypeMeta.Kind)
-	//fmt.Println(rev.ObjectMeta.Name)
-	//fmt.Println(rev.Spec.PodSpec)
-	//fmt.Println(reflect.TypeOf(rev.Spec.PodSpec))
-	// set pod name from revision's parent service name
-	//pod_1.Name = rev.Labels['serving.knative.dev/service']
-	//pod_1.Name := rev.Labels["serving.knative.dev/service"].(string)
-	/*
-		for key, value := range rev.Labels {
-			fmt.Println("Key:", key, "Value:", value)
-		}*/
+	// get service name from revision
 	s_name := rev.Labels["serving.knative.dev/service"]
-	//fmt.Println(s_name)
 
 	pod_1.Name = s_name
 	pod_1.Spec.Containers[0].Name = s_name
-	//fmt.Println(pod_1.Name)
-	//fmt.Println(pod_1)
-
-	// convert new pod to YAML
-	/*
-		pod_1_yaml, err := Yml.Marshal(pod_1)
-		if err != nil {
-			fmt.Printf("err: %v\n", err)
-			return
-		}
-		//fmt.Println(string(pod_1_yaml))*/
 
 	// hard code the apiVersion & kind
 	dep_1.APIVersion = "apps/v1"
@@ -81,39 +57,17 @@ func generateDeploymentSpec(stream []uint8) string {
 
 	// selector & labeling setup for deployment
 	app_label_requirement, _ := labels.NewRequirement("app", selection.Equals, []string{s_name})
-	//app_label_requirement_v1 := v1.LabelSelectorRequirement("app", selection.Equals, s_name) //{}
-	//app_label_requirement_v1 := v1.LabelSelectorRequirement{}
-	//app_label_requirement_v1.Key = "app"
-	//app_label_requirement_v1.Operator = v1.LabelSelectorOperator(selection.Equals)
-	//app_label_requirement_v1.Values = []string{s_name}
-	//app_label_requirement_v1.
 	app_label_selector := labels.NewSelector()
 	app_label_selector_v1 := v1.LabelSelector{}
 	label_map := make(map[string]string)
 	app_label_selector_v1.MatchLabels = label_map
-	//app_label_selector_v1.MatchExpressions = append(app_label_selector_v1.MatchExpressions, app_label_requirement_v1)
 
 	app_label_selector = app_label_selector.Add(*app_label_requirement)
 
-	// set selector
-	//label_map := make(map[string]string)
-	//println(reflect.TypeOf(&dep_1.Spec.Selector))
-	//label_selector, err := lz.NewRequirement("app", selection.Equals, []string{s_name})
-
-	//dep_1.Spec.Selector = v1.SetAsLabelSelector(label_map)
-	//dep_1.Spec.Selector.MatchExpressions[0] = v1.LabelSelectorRequirement{}
 	dep_1.Spec.Selector = &app_label_selector_v1
 
-	//dep_1.Spec.Selector.MatchLabels = label_map
-	//dep_1.Spec.Selector = lbls.NewRequirement("app", selection.Equals, []string{s_name})
-	//dep_1.Spec.Selector.MatchExpressions[0] = *label_selector
-	//dep_1.Spec.Selector.MatchLabels["app"] = s_name
-	//fmt.Println(dep_1.Spec.Template.ObjectMeta.Labels)
-
-	//container_map := make(map[int32]corev1.Container)
 	dep_1.Spec.Template.ObjectMeta.Labels = label_map
 	dep_1.Spec.Template.ObjectMeta.Labels["app"] = s_name
-	//fmt.Println(dep_1.Spec.Template.Spec.Containers)
 
 	// create some annotations for the deployment's template metadata that indicate the source kn revision
 	annotation_map := make(map[string]string)
@@ -137,7 +91,6 @@ func generateDeploymentSpec(stream []uint8) string {
 
 func main() {
 
-	// spec := &knative.spec{}
 	stream, err := ioutil.ReadFile("samples/hello_revision_4.yaml")
 	fmt.Println(reflect.TypeOf(stream))
 	if err != nil {
@@ -146,5 +99,4 @@ func main() {
 
 	fmt.Println(generateDeploymentSpec(stream))
 
-	//fmt.Println(dep_1)
 }
